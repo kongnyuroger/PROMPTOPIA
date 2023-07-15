51213be1563fc5fe04bbaa56f9fc3c40c67353d7
Kongnyu Roger <kongnyuroger711@gmail.com>
2023-07-15 11:47:19 +0100

    created home page

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+
+
+const Home = () => {
+  return (
+    <section className = 'w-full flex-center flex-col'>
+        <h1 className = 'head_text text-center'>
+            Discover and Share
+            <br />
+            <span className = 'orange_gradient text-center'> AI Powered Prompts</span>
+        </h1>
+        <p className ='desc text-center'>promptopia ia an open-souce AI prompting tools for morden world
+            to discover, creat and share creative prompts
+        </p>
+    </section>
+  )
+}
+
+export default Home
